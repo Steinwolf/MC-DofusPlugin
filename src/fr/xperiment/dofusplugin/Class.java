33885b1670c6b9b576c405b91dd15b67bd34ad33
Steinwolf <pierreloup.martigne@viacesi.fr>
2020-08-11 00:33:15 +0200

    Class Enum

---
+++ src/fr/xperiment/dofusplugin/Class.java
@@ -0,0 +1,12 @@
+package fr.xperiment.dofusplugin;
+
+public enum Class {
+	SACRIEUR,
+	CRA,
+	IOP,
+	XELOR,
+	SRAM,
+	OSAMODAS,
+	ENIRIPSA,
+	ROUBLARD
+}
--- src/fr/xperiment/dofusplugin/commands/CommandsExecutor.java
+++ src/fr/xperiment/dofusplugin/commands/CommandsExecutor.java
@@ -27,15 +27,18 @@ public class CommandsExecutor implements CommandExecutor {
 			}
 			Inventory classMenu = Bukkit.createInventory(null, 27, "§8Menu des classes disponibles");
 			
-			classMenu.setItem(1, CustomItems.getItem(Material.ARROW,"§2Cra",Arrays.asList("§6[Dofus]","Cliquez pour choisir la classe §2Cra§r !")));
-			classMenu.setItem(3, CustomItems.getItem(Material.SPAWNER,"§2Osamodas",Arrays.asList("§6[Dofus]","Cliquez pour choisir la classe §2Osamodas§r !")));
-			classMenu.setItem(5, CustomItems.getItem(Material.TNT,"§2Roublard",Arrays.asList("§6[Dofus]","Cliquez pour choisir la classe §2Roublard§r !")));
-			classMenu.setItem(7, CustomItems.getItem(Material.WITHER_SKELETON_SKULL,"§2Sram",Arrays.asList("§6[Dofus]","Cliquez pour choisir la classe §2Sram§r !")));
+			classMenu.setItem(1, CustomItems.getItem(Material.CHAIN,"§2Sacrieur",Arrays.asList("§6[Dofus]","Cliquez pour choisir la classe §2Sacrieur§r !")));
+			classMenu.setItem(3, CustomItems.getItem(Material.ARROW,"§2Cra",Arrays.asList("§6[Dofus]","Cliquez pour choisir la classe §2Cra§r !")));
+			classMenu.setItem(5, CustomItems.getItem(Material.IRON_SWORD,"§2Iop",Arrays.asList("§6[Dofus]","Cliquez pour choisir la classe §2Iop§r !")));
+			//classMenu.setItem(7, CustomItems.getItem(Material.CLOCK,"§2Xelor",Arrays.asList("§6[Dofus]","Cliquez pour choisir la classe §2Xelor§r !")));
+			classMenu.setItem(7, CustomItems.getItem(Material.BARRIER,"§2Xelor",Arrays.asList("§6[Dofus]","Cliquez pour choisir la classe §2Xelor§r !")));
 			
-			classMenu.setItem(19, CustomItems.getItem(Material.CHAIN,"§2Sacrieur",Arrays.asList("§6[Dofus]","Cliquez pour choisir la classe §2Sacrieur§r !")));
-			classMenu.setItem(21, CustomItems.getItem(Material.CLOCK,"§2Xelor",Arrays.asList("§6[Dofus]","Cliquez pour choisir la classe §2Xelor§r !")));
-			classMenu.setItem(23, CustomItems.getItem(Material.POTION,"§2WIP",Arrays.asList("§6[Dofus]","Cliquez pour choisir la classe §2Eniripsa§r !")));
-			classMenu.setItem(25, CustomItems.getItem(Material.IRON_SWORD,"§2Iop",Arrays.asList("§6[Dofus]","Cliquez pour choisir la classe §2Iop§r !")));
+			classMenu.setItem(19, CustomItems.getItem(Material.WITHER_SKELETON_SKULL,"§2Sram",Arrays.asList("§6[Dofus]","Cliquez pour choisir la classe §2Sram§r !")));
+			classMenu.setItem(21, CustomItems.getItem(Material.SPAWNER,"§2Osamodas",Arrays.asList("§6[Dofus]","Cliquez pour choisir la classe §2Osamodas§r !")));
+			//classMenu.setItem(23, CustomItems.getItem(Material.POTION,"§2WIP",Arrays.asList("§6[Dofus]","Cliquez pour choisir la classe §2Eniripsa§r !")));
+			classMenu.setItem(23, CustomItems.getItem(Material.BARRIER,"§2WIP",Arrays.asList("§6[Dofus]","Cliquez pour choisir la classe §2Eniripsa§r !")));
+			//classMenu.setItem(25, CustomItems.getItem(Material.TNT,"§2Roublard",Arrays.asList("§6[Dofus]","Cliquez pour choisir la classe §2Roublard§r !")));
+			classMenu.setItem(25, CustomItems.getItem(Material.BARRIER,"§2Roublard",Arrays.asList("§6[Dofus]","Cliquez pour choisir la classe §2Roublard§r !")));
 			
 			player.openInventory(classMenu);
 		}
